f47b69cfaaffff03496f59aa055155018cb93c07
jiangrongtao <1183461018@qq.com>
2017-03-31 17:43:31 +0800

    添加效果图10和11

---
+++ AndroidDemoGather/BottomNavigationTest/src/main/java/com/jrt/bottomnavigation/adapter/MyPageAdapter.java
@@ -0,0 +1,11 @@
+package com.jrt.bottomnavigation.adapter;
+
+/**
+ * 描述：AndroidDemoGather
+ * 开发者：开发者的乐趣JRT
+ * 创建时间：2017-3-31 16:48
+ * CSDN地址：http://blog.csdn.net/Jiang_Rong_Tao/article
+ * E-mail：jrtxb520@163.com
+ **/
+public class MyPageAdapter {
+}
