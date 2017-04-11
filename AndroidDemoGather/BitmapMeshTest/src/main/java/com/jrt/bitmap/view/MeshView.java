0c8afef9609606e712656e4848a9cb8792c31017
jiangrongtao <1183461018@qq.com>
2017-04-11 15:29:33 +0800

    添加前后摄像头的切换，修改只能拍一张照片的bug

---
+++ AndroidDemoGather/BitmapMeshTest/src/main/java/com/jrt/bitmap/view/MeshView.java
@@ -0,0 +1,11 @@
+package com.jrt.bitmap.view;
+
+/**
+ * 描述：AndroidDemoGather
+ * 开发者：开发者的乐趣JRT
+ * 创建时间：2017-4-7 10:21
+ * CSDN地址：http://blog.csdn.net/Jiang_Rong_Tao/article
+ * E-mail：jrtxb520@163.com
+ **/
+public class MeshView {
+}
